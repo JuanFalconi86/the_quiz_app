083b4c31c3b8deb26f13f571b83800d770510119
Juan Falconi <juanfalconi86@gmail.com>
2021-02-17 17:36:25 +0100

    did some style modifs

--- index.js
+++ index.js
@@ -37,20 +37,77 @@ let listOfQuestions = [
     ],
     correctAnswer: "David Lynch",
   },
+  {
+    nameOfQuestion: "In what year was the movie GoodFellas released ?",
+    answerOptions: ["1982", "1990", "2004", "1970"],
+    correctAnswer: "1990",
+  },
+  {
+    nameOfQuestion:
+      "What is the name of Mark Whalberg's character in Boogie Nights ? ",
+    answerOptions: ["Little Sam", "Doctor Love", "Dirk Diggler", "Mr Simmons"],
+    correctAnswer: "Dirk Diggler",
+  },
+  {
+    nameOfQuestion:
+      "In There Will Be Blood, what is Daniel Day Lewis character name ?",
+    answerOptions: [
+      "Daniel Plainview",
+      "Max Wallace",
+      "Simon Foster",
+      "Christian Levington",
+    ],
+    correctAnswer: "Daniel Plainview",
+  },
+  {
+    nameOfQuestion: "What drugs is Walter White cooking in Breaking Bad ?",
+    answerOptions: ["Cocaine", "Anthrax", "Methamphetamines", "Heroine"],
+    correctAnswer: "Methamphetamines",
+  },
+  {
+    nameOfQuestion: "What is Michael Scott's position in The Office ? ",
+    answerOptions: [
+      "CTO",
+      "Marketing Manager",
+      "HR Manager",
+      "Regional Manager",
+    ],
+    correctAnswer: "Regional Manager",
+  },
+  {
+    nameOfQuestion: "How many FRIENDS are there ? ",
+    answerOptions: ["Six", "Ten", "Four", "Seven"],
+    correctAnswer: "Six",
+  },
+  {
+    nameOfQuestion: "What is Homer's J. Simpson second name ?",
+    answerOptions: ["James", "Jhon", "Jay", "Jules"],
+    correctAnswer: "Jay",
+  },
+  {
+    nameOfQuestion: "In Mr Robot, what is Elliot Anderson's profession ?",
+    answerOptions: [
+      "Cybersecurity Engineer",
+      "Software Engineer",
+      "UX Designer",
+      "Freelance",
+    ],
+    correctAnswer: "Cybersecurity Engineer",
+  },
 ];
 
 // questionDiv.innerHTML = question1.nameOfQuestion // testing if the question appears in the browser, in the DOM ==> it works
 
 // THIS FUNCTION WILL SHOW A QUESTION
 function showQuestion(question) {
-  checkIfLast(question);
+  checkIfLast(question); //here I am calling a function that checks if the current question is the last one...
 
-  questionDiv.innerHTML = question.nameOfQuestion;
+  questionDiv.innerHTML = question.nameOfQuestion; // here I am pushing the array's object value to the HTML to have it as a question
   options.forEach((option, index, arr) => {
-    option.innerHTML = question.answerOptions[index];
+    option.innerHTML = question.answerOptions[index]; // here, I am pushing the answer options into the HTML
     option.parentElement.style.pointerEvents = "auto"; //here, I am "reseting" the pointer events so when I see the question, I can click everywhere
     option.classList.remove("right"); //again, reseting the class, because in the previous question, there is a class applied (because someone clicked)
-    option.classList.remove("wrong"); //the same that above
+    option.classList.remove("wrong"); //the same that above, I am reseting the class
   });
 }
 
@@ -60,6 +117,7 @@ function checkIfLast(question) {
   if (listOfQuestions.indexOf(question) === listOfQuestions.length - 1) {
     replaceNextButton()
   }
+  
 }
 
 // THIS FUNCTION WILL REPLACE THE NEXT BUTTON FOR ANOTHER BUTTON
@@ -81,7 +139,7 @@ function rightOrWrong(oneQuestion, option) {
   if (selectedAnswer === oneQuestion.correctAnswer) {
     option.classList.add("right");
     score++;
-    scoreText.innerHTML = `Ton score est de ${score} points pour l'instant`
+    scoreText.innerHTML = `Your score is of ${score} points`
     alert("Correct answer!! Nicely done");
   } else {
     option.classList.add("wrong");
@@ -99,16 +157,8 @@ options.forEach((option) => {
   };
 });
 
-// UUNE AUTRE ALTERNATIVE
-// function onOptionClicked(event) {
-//   rightOrWrong(listOfQuestions[currentQuestionIndex], event.target);
-//   options.forEach((option, index, arr) => {
-//     option.removeEventListener("click", onOptionClicked);
-//   });
-// }
 
-//CREATE A FUNCTION THAT ADDS A BUTTON AND SENDS TO A FINAL PAGE
-
+//CALLING THE FUNCTIONS TO WORK
 showQuestion(listOfQuestions[currentQuestionIndex]);
 endButton.style.visibility = "hidden"
 
@@ -118,3 +168,5 @@ endButton.style.visibility = "hidden"
 //     showQuestion(listOfQuestions[currentQuestionIndex]);
 
 // }, 7000);
+
+console.log(listOfQuestions.length)
